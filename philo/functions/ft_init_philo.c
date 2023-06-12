4ddb89a15bbc9f7fc53025c2fd22abe1bd6e9c83
Marouane Mounib <mamounib@e2r7p11.1337.ma>
2023-06-12 08:04:33 +0100

    move file function to philo/

--- philo/functions/ft_init_philo.c
+++ philo/functions/ft_init_philo.c
@@ -6,11 +6,13 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 23:13:39 by mamounib          #+#    #+#             */
-/*   Updated: 2023/06/07 01:54:43 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/06/12 07:52:15 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../manda/main.h"
+#include "../main.h"
+#include <pthread.h>
+#include <sys/_pthread/_pthread_mutex_t.h>
 
 t_philo	*ft_new_philo(t_info info)
 {
@@ -20,7 +22,8 @@ t_philo	*ft_new_philo(t_info info)
 	if (!philo)
 		return (NULL);
 	philo->next = NULL;
-	philo->info = info;
+	philo->info = &info;
+	pthread_mutex_init(&philo->fork, NULL);
 	return (philo);
 }
 
@@ -33,6 +36,13 @@ void	ft_add_philo(t_philo *philos, t_philo *philo)
 	philo->next = philos->first;
 	philos->last = philo;
 	philos->n_philo ++;
+}
+
+void	init_info(char **argv, t_info *info)
+{
+	info->time_to_die = atoi(argv[2]);
+	info->time_to_eat = atoi(argv[3]);
+	info->time_to_sleep = atoi(argv[4]);
 }
 
 t_philo	*ft_init_philos(t_philo *philos,t_info info,int nbr)
---
+++ philo/functions/ft_routine.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_routine.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 22:52:42 by mamounib          #+#    #+#             */
+/*   Updated: 2023/06/12 07:49:34 by mamounib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../main.h"
+#include <pthread.h>
+#include <stdio.h>
+
+//time to eat
+//time to die
+// esting 
+// sleeping
+// thinking 
+// times eating
+// and check the death 
+// 1 us
+// 1 ms 
+// usleep(time(ms) * 1000)
+// sleep 
+long	ft_get_time(struct timeval time)
+{
+
+	return ((time.tv_sec / 1000) + (time.tv_usec * 1000));
+}
+
+void	ft_eat(t_philo *philo)
+{
+	long	time;
+	struct	timeval	date;
+	int x = 1;
+
+	time = ft_get_time(date);
+	pthread_mutex_lock(&philo->next->fork);
+	pthread_mutex_lock(&philo->fork);
+		printf("%ld %d has taken a fork", time, philo->n_philo);
+		printf("%ld %d has taken a fork", time, philo->n_philo);
+	
+	printf("%ld %d is eating", time, philo->n_philo);
+}
+
+void	ft_sleep(t_philo *philo)
+{
+	long	time;
+
+	time = 0;
+	printf("%ld %d is sleeping", time, philo->n_philo);
+}
+
+void	ft_think(t_philo *philo)
+{
+	long	time;
+
+	time = 0;
+	printf("%ld %d is thinking", time, philo->n_philo);
+}
+
+void	ft_routine(t_philo *philo, char **argv)
+{
+	ft_eat(philo);
+	ft_sleep(philo);
+	ft_think(philo);
+}
