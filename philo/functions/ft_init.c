cbc6fe277198821165d845e33d470e646ecb62d9
MOUNIB MAROUANE <marouan.mounib33@gmail.com>
2023-07-29 15:54:58 +0100

    initialize philos

--- philo/functions/ft_init.c
+++ philo/functions/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 15:49:31 by mamounib          #+#    #+#             */
-/*   Updated: 2023/07/25 16:35:52 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/07/29 15:23:07 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,41 @@
 
 #include "../main.h"
 
-t_philo	*ft_new_philo(void)
+t_philo	*ft_new_philo()
 {
 	t_philo	*philo;
 	
 	philo = (t_philo *)malloc(sizeof(t_philo *));
+	// pthread_mutex_init(&philo->fork, NULL);
 	philo->next = NULL;
-	pthread_mutex_init(&philo->fork, NULL);
 	return (philo);
 }
 
-t_philo	*init_philos(int nbr)
+t_philo	*ft_init_philos(int nbr)
 {
-	t_philo *current;
-	t_philo *first;
-	
-	while (nbr--)
+	t_philo	*first;
+	t_philo	*current;
+	int		i;
+
+	first  = NULL;
+	i = nbr;
+	while (i)
 	{
-		current = ft_new_philo();
-		if (first == NULL)
+		puts("philo");
+		if( i == nbr)
+		{
+			current = ft_new_philo();
 			first = current;
+			// pthread_mutex_init(&current->fork, NULL);
+		}
 		else
 		{
 			current->next = ft_new_philo();
 			current = current->next;
+			// pthread_mutex_init(&current->fork, NULL);	
 		}
-		current->next = first;
+		i--;
 	}
+	current->next = first;
 	return (first);
 }
