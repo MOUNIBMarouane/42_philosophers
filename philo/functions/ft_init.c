68d204bfa89251308f289b7519daec0a4b986c09
aamhamdi <original@ORIGINALs-MacBook-Air.local>
2023-08-31 11:15:41 +0100

    add sleep and think

--- philo/functions/ft_init.c
+++ philo/functions/ft_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
+/*   By: original <original@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 15:49:31 by mamounib          #+#    #+#             */
-/*   Updated: 2023/08/26 16:24:34 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/08/31 10:40:50 by original         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,6 @@ t_philo	*ft_new_philo(int id, t_info *info)
 	philo->next = NULL;
 	return (philo);
 }
-// void	ft_addback(t_philo **philo, t_philo *node)
-// {
-	
-// }
 
 t_philo	*ft_init_philos(int nbr, t_info *info)
 {
@@ -46,17 +42,13 @@ t_philo	*ft_init_philos(int nbr, t_info *info)
 		{
 			first = ft_new_philo(i, info);
 			current = first;
-			
 		}
 		else
 		{
 			current->next = ft_new_philo(i, info);
 			current = current->next;
 		}
-		printf("philo %d\n", current->id_philo);
-	}
-	printf("philo %d\n", current->id_philo);
-	
+	}	
 	current->next = first;
 	return (first);
 }
@@ -71,27 +63,13 @@ void ft_start(t_philo *philo, t_info *info)
 {
 	int	i;
 
-	t_philo *first;
-	first = philo;
 	i = info->nbr_philo;
 	info->start_time = init_time();
 	pthread_mutex_init(&info->msg, NULL);
-	info->start_time = init_time();
 	while (i-- >= 1)
 	{
-		pthread_mutex_init(&philo->fork, NULL);
-		philo = philo->next;
-	}
-	philo = first;
-	i = info->nbr_philo;
-	while (i-- >= 1)
-	{
-		if (philo->id_philo % 2 == 0)
-		usleep(50);
-		// pthread_mutex_init(&philo->fork);
 		pthread_create(&philo->thread, NULL, ft_routine, philo);
 		pthread_detach(philo->thread);
-		usleep(50);
 		philo = philo->next;
 	}
 }
--- philo/functions/ft_routine.c
+++ philo/functions/ft_routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_routine.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
+/*   By: original <original@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 22:52:42 by mamounib          #+#    #+#             */
-/*   Updated: 2023/08/26 16:16:18 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/08/31 11:13:26 by original         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,48 @@
 #include <pthread.h>
 #include <stdio.h>
 
-/**
- * @brief eat routine
- * 
- * @param philo 
- * 1 - locked his fock and print
- * 2 - locked next forck and print 
- * 3 - sleep time eat
- * 4 - init last meal 
- * 5 - 2 unlock
- */
-
-void	ft_msg(char *msg, t_philo *philo, int unlock)
+void	ft_msg(char *msg, t_philo *philo)
 {
 	long long	time;
 
 	pthread_mutex_lock(&philo->info->msg);
 	time = init_time() - philo->info->start_time;
 	printf("%lld %d %s\n", time, philo->id_philo, msg);
-	if (unlock)
-		pthread_mutex_unlock(&philo->info->msg);
+	pthread_mutex_unlock(&philo->info->msg);
 }
 void	ft_eat(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork);
-	ft_msg("has taken a fork",philo, 1);
+	ft_msg("has taken a fork",philo);
 	pthread_mutex_lock(&philo->next->fork);
-	ft_msg("has taken a fork",philo, 1);
+	ft_msg("has taken a fork",philo);
 	philo->last_eat = init_time();
-	ft_msg("is eating", philo, 1);
+	ft_msg("is eating", philo);
 	usleep(philo->info->time_to_eat);
 	pthread_mutex_unlock(&philo->fork);
 	pthread_mutex_unlock(&philo->next->fork);
-	ft_msg("is sleeping", philo, 1);
+}
+void	ft_sleep(t_philo *philo)
+{
+	ft_msg("is sleeping", philo);
 	usleep(philo->info->time_to_sleep);
+}
+void	ft_think(t_philo *philo)
+{
+	ft_msg("is thinking", philo);
 }
 
 void *ft_routine(void *philo)
 {
 	t_philo *phil;
-
 	phil = (t_philo *)philo;
+	if (phil->id_philo % 2 == 0)
+		usleep(400);
 	while (1)
 	{
 		ft_eat(phil);
+		ft_sleep(phil);
+		ft_think(phil);
 	}
 	return ((void *)NULL);
 }
